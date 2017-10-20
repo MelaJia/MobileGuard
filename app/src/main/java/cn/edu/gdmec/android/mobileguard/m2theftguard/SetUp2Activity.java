e723bd47a7a55b660ede3b0e93cd7dd95219ec40
pp <2426759858@qq.com>
2017-10-20 18:25:28 +0800

    手机安全号码绑定

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/SetUp2Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/SetUp2Activity.java
@@ -15,47 +15,56 @@ import cn.edu.gdmec.android.mobileguard.R;
  * Created by HP on 2017/10/11.
  */
 
-public class SetUp2Activity extends BaseSetUpActivity {
-//    private TelephonyManager mTelephonyManager;
-//    private Button mBindSTMBtn;
+public class SetUp2Activity extends BaseSetUpActivity implements View.OnClickListener{
+    private TelephonyManager mTelephonyManager;
+    private Button mBindSTMBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up2);
         ((RadioButton) findViewById(R.id.rb_second)).setChecked(true);
-//        mTelephonyManager=(TelephonyManager)getSystemService(TELEPHONY_SERVICE);
-//        initView();
-
+        mTelephonyManager=(TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+        ((RadioButton)findViewById(R.id.rb_second)).setChecked(true);
+        mBindSTMBtn = (Button)findViewById(R.id.btn_bind_sim);
+        mBindSTMBtn.setOnClickListener(this);
+        if(isBind()){
+            mBindSTMBtn.setEnabled(false);
+        }else {
+            mBindSTMBtn.setEnabled(true);
+        }
     }
+
+
 
     /*
     * initView（）方法初始化控件
     * */
-//    private void initView(){
-//        ((RadioButton)findViewById(R.id.rb_second)).setChecked(true);
-//        mBindSTMBtn = (Button)findViewById(R.id.btn_bind_sim);
-//        mBindSTMBtn.setOnClickListener(this);
-//        if(isBind()){
-//            mBindSTMBtn.setEnabled(false);
-//        }else {
-//            mBindSTMBtn.setEnabled(true);
-//        }
-//    }
-//
-//    private boolean isBind(){
-//        String simString = sp.getString("sim", null);
-//        if (TextUtils.isEmpty(simString)){
-//            return false;
-//        }
-//        return true;
-//    }
+ /* private void initView(){
+//        设置第二个小圆点颜色
+((RadioButton)findViewById(R.id.rb_second)).setChecked(true);
+        mBindSTMBtn = (Button)findViewById(R.id.btn_bind_sim);
+        mBindSTMBtn.setOnClickListener(this);
+        if(isBind()){
+            mBindSTMBtn.setEnabled(false);
+       }else {
+           mBindSTMBtn.setEnabled(true);
+        }
+   }*/
+
+    private boolean isBind(){
+        String simString = sp.getString("sim", null);
+        if (TextUtils.isEmpty(simString)){
+            return false;
+        }
+        return true;
+    }
     @Override
     public void showNext() {
-//        if (!isBind()){
-//            Toast.makeText(this,"您还没有绑定sim卡！",0).show();
-//            return;
-//        }
+        if (!isBind()){
+            Toast.makeText(this,"您还没有绑定sim卡！",Toast.LENGTH_LONG).show();
+            return;
+        }
         startActivityAndFinishSelf(SetUp3Activity.class);
     }
 
@@ -64,34 +73,34 @@ public class SetUp2Activity extends BaseSetUpActivity {
         startActivityAndFinishSelf(SetUp1Activity.class);
 
     }
-}
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()){
-//            case R.id.btn_bind_sim:
-//                //绑定sim卡
-//                bindSIM();
-//                break;
-//        }
-//    }
-//
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_bind_sim:
+                //绑定sim卡
+                bindSIM();
+                break;
+        }
+    }
+    //
 //    /**
 //     * 绑定sim卡
 //     * */
-//    private void bindSIM(){
-//        if (!isBind()){
-//            String simSerialNumber = mTelephonyManager.getSimSerialNumber();
-//            SharedPreferences.Editor edit = sp.edit();
-//            edit.putString("sim",simSerialNumber);
-//            edit.commit();
-//            Toast.makeText(this,"SIM卡绑定成功！",0).show();
-//            mBindSTMBtn.setEnabled(false);
-//        }else {
-//            //已经绑定，提醒用户
-//            Toast.makeText(this,"SIM卡已经绑定！",0).show();
-//            mBindSTMBtn.setEnabled(false);
-//        }
-//    }
+    private void bindSIM(){
+        if (!isBind()){
+            String simSerialNumber = mTelephonyManager.getSimSerialNumber();
+            SharedPreferences.Editor edit = sp.edit();
+            edit.putString("sim",simSerialNumber);
+            edit.commit();
+            Toast.makeText(this,"SIM卡绑定成功！",Toast.LENGTH_LONG).show();
+            mBindSTMBtn.setEnabled(false);
+        }else {
+            //已经绑定，提醒用户
+            Toast.makeText(this,"SIM卡已经绑定！",Toast.LENGTH_LONG).show();
+            mBindSTMBtn.setEnabled(false);
+        }
+    }
 
-
+}
 
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/SetUp3Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/SetUp3Activity.java
@@ -15,7 +15,7 @@ import cn.edu.gdmec.android.mobileguard.R;
  * Created by HP on 2017/10/11.
  */
 
-public class SetUp3Activity extends BaseSetUpActivity{
+public class SetUp3Activity extends BaseSetUpActivity implements View.OnClickListener{
     private EditText mInputPhone;
 
     @Override
@@ -24,9 +24,25 @@ public class SetUp3Activity extends BaseSetUpActivity{
         setContentView(R.layout.activity_set_up3);
         //设置第3个小圆点的颜色
         ((RadioButton) findViewById(R.id.rb_third)).setChecked(true);
+        findViewById(R.id.btn_addcontact).setOnClickListener(this);
+        mInputPhone = (EditText) findViewById(R.id.et_inputphone);
+        String safephone = sp.getString("safephone", null);
+        if (!TextUtils.isEmpty(safephone)) {
+            mInputPhone.setText(safephone);
+        }
     }
+
     @Override
     public void showNext(){
+        //判断文本输入框中是否有电话号码
+        String safePhone = mInputPhone.getText().toString().trim();
+        if(TextUtils.isEmpty(safePhone)){
+            Toast.makeText(this,"请输入安全号码",Toast.LENGTH_LONG).show();
+            return;
+        }
+        SharedPreferences.Editor edit = sp.edit();
+        edit.putString("safephone",safePhone);
+        edit.commit();
         startActivityAndFinishSelf(SetUp4Activity.class);
     }
 
@@ -34,63 +50,22 @@ public class SetUp3Activity extends BaseSetUpActivity{
     public void showPre(){
         startActivityAndFinishSelf(SetUp2Activity.class);
     }
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.btn_addcontact:
+                startActivityForResult(new Intent(this,ContactSelectActivity.class),0);
+                break;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(data!=null){
+            String phone = data.getStringExtra("phone");
+            mInputPhone.setText(phone);
+        }
+    }
 }
 
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_set_up3);
-//        initView();
-//    }
-//    /**
-//     *
-//     */
-//    private void initView(){
-//        ((RadioButton)findViewById(R.id.rb_third)).setChecked(true);
-//        findViewById(R.id.btn_addcontact).setOnClickListener(this);
-//        mInputPhone = (EditText)findViewById(R.id.et_inputphone);
-//        String safephone = sp.getString("safephone",null);
-//        if (!TextUtils.isEmpty(safephone)){
-//            mInputPhone.setText(safephone);
-//        }
-//
-//    }
-//
-//    @Override
-//    public void showNext() {
-//        //判断文本输入框是否有电话号码
-//        String safePhone = mInputPhone.getText().toString().trim();
-//        if (TextUtils.isEmpty(safePhone)){
-//            Toast.makeText(this,"请输入安全号码", 0).show();
-//            return;
-//        }
-//        SharedPreferences.Editor edit = sp.edit();
-//        edit.putString("safephone",safePhone);
-//        edit.commit();
-//        startActivityAndFinishSelf(SetUp4Activity.class);
-//
-//    }
-//
-//    @Override
-//    public void showPre() {
-//        startActivityAndFinishSelf(SetUp2Activity.class);
-//
-//    }
-//
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()){
-//            case R.id.btn_addcontact:
-//                startActivityForResult(new Intent(this,ContactSelectActivity.class), 0);
-//                break;
-//        }
-//
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (data!=null){
-//            String phone = data.getStringExtra("phone");
-//            mInputPhone.setText(phone);
-//        }
-//    }
-//}
+
