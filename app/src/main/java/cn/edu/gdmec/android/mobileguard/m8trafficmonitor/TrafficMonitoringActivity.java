a1146a0271b9d6e38962524894bcfa9dd3e52328
pp <2426759858@qq.com>
2017-11-28 10:03:12 +0800

    第八章

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/TrafficMonitoringActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m8trafficmonitor;
+
+/**
+ * Created by student on 17/11/28.
+ */
+
+public class TrafficMonitoringActivity {
+}
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/service/TrafficMonitoringService.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/service/TrafficMonitoringService.java
@@ -1,8 +1,95 @@
 package cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service;
 
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.TrafficStats;
+import android.os.IBinder;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.db.dao.TrafficDao;
+
 /**
  * Created by HP on 2017/11/23.
  */
 
-public class TrafficMonitoringService {
+public class TrafficMonitoringService extends Service{
+    private long mOldRxBytes;
+    private long mOldTxBytes;
+    private TrafficDao dao;
+    private SharedPreferences mSP;
+    private long usedFlow;
+    boolean flag = true;
+
+    @Override
+    public IBinder onBind(Intent intent){
+        return null;
+    }
+    @Override
+    public void onCreate() {
+     super.onCreate();
+        mOldRxBytes = TrafficStats.getMobileRxBytes();
+        mOldTxBytes = TrafficStats.getMobileTxBytes();
+        dao = new TrafficDao(this);
+        mSP = getSharedPreferences("config",MODE_PRIVATE);
+        mThread.start();
+    }
+    private Thread mThread = new Thread(){
+        public void run(){
+            while (flag){
+                try{
+                    Thread.sleep(2000 * 60);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                updateTodayGPRS();
+            }
+        }
+        private void updateTodayGPRS(){
+            usedFlow = mSP.getLong("userflow",0);
+            Date date = new Date();
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(date);
+            if (calendar.DAY_OF_MONTH == 1 & calendar.HOUR_OF_DAY == 0 & calendar.MINUTE < 1 & calendar.SECOND <30){
+                usedFlow = 0;
+            }
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String dataString = sdf.format(date);
+            long moblieGPRS = dao.getMoblieGRPS(dataString);
+            long moblieRxBytes = TrafficStats.getMobileRxBytes();
+            long moblieTxBytes = TrafficStats.getMobileTxBytes();
+            long newGprs = (moblieRxBytes + moblieTxBytes) - mOldRxBytes - mOldTxBytes;
+            mOldRxBytes = moblieRxBytes;
+            mOldTxBytes = moblieTxBytes;
+            if (newGprs < 0){
+                newGprs = moblieRxBytes + moblieTxBytes;
+            }
+            if (moblieGPRS == -1){
+                dao.insertTodayGPRS(newGprs);
+            }else{
+                if (moblieGPRS <0){
+                    moblieGPRS = 0;
+                }
+                dao.UpdateTodayGPRS(moblieGPRS + newGprs);
+            }
+            usedFlow = usedFlow + newGprs;
+            SharedPreferences.Editor edit = mSP.edit();
+            edit.putLong("usedFlow",usedFlow);
+            edit.commit();
+
+        };
+
+    };
+    @Override
+    public void onDestroy(){
+        if(mThread != null & !mThread.interrupted()){
+            flag = false;
+            mThread.interrupt();
+            mThread = null;
+        }
+        super.onDestroy();
+    }
 }
