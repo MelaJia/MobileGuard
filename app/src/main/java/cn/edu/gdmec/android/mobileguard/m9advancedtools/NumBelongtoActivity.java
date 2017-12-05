da7f68f86b3239a0f91a59d9ab18bbbb5b8bfbd8
MelaJia <838672863@qq.com>
2017-12-05 10:00:35 +0800

    高级修改

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/NumBelongtoActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/NumBelongtoActivity.java
@@ -1,15 +1,24 @@
 package cn.edu.gdmec.android.mobileguard.m9advancedtools;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import cn.edu.gdmec.android.mobileguard.R;
 
-public class NumBelongtoActivity extends AppCompatActivity {
+public class NumBelongtoActivity extends Activity {
+    private EditText mNumET;
+    private TextView mResultTV;
+    private String dbName = "address.db";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_num_belongto);
+
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/widget/AdvancedToolsView.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/widget/AdvancedToolsView.java
@@ -5,9 +5,12 @@ import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.StrictMode;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import cn.edu.gdmec.android.mobileguard.R;
 
 /**
  * Created by student on 17/12/4.
@@ -21,11 +24,29 @@ public class AdvancedToolsView extends RelativeLayout{
 
     public AdvancedToolsView(Context context){
         super(context);
+        init(context);
 
 
     }
     public AdvancedToolsView(Context context, AttributeSet attrs,int defStyle){
         super(context,attrs,defStyle);
+        //拿到属性对象值
+        TypedArray mTypeArray = context.obtainStyledAttributes(attrs, R.styleable.AdvancedToolsView);
+        desc = mTypeArray.getString(R.styleable.AdvancedToolsView_desc);
+        drawable = mTypeArray.getDrawable(R.styleable.AdvancedToolsView_android_src);
+        mTypeArray.recycle();
+        init(context);
 
+
+
+    }
+    private void init(Context context){
+        //将资源转化成view对象显示在自己身上
+        View view = View.inflate(context,R.layout.ui_advancedtools_view,null);
+        this.addView(view);
+        mDescriptionTV = (TextView)findViewById(R.id.tv_description);
+        mLeftImgv = (ImageView)findViewById(R.id.imgv_left);
+        mDescriptionTV.setText(desc);
+        if (drawable != null)mLeftImgv.setImageDrawable(drawable);
     }
 }
